8f67b1b76ac3a0e112f26b8df937f5ad8ae8b922
Roettges Florian <roettges.florian@scheidt-bachmann.de>
2023-12-01 10:15:12 +0100

    added enhanced logging part 2

--- configparser.go
+++ configparser.go
@@ -174,7 +174,7 @@ func parseCertFromFile(certFile string) (*x509.Certificate, error) {
 	}
 	certificate, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return nil, fmt.Errorf("could not parse certificate from file #{certFile}", err)
+		return nil, fmt.Errorf("could not parse certificate from file #{certFile} %v", err)
 	}
 	return certificate, nil
 }
